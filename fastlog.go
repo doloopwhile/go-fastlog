60b2496e974d80a1efef1c4bce2964cd4916e211
OMOTO Kenji <kenji.omoto@hde.co.jp>
2014-08-08 11:26:15 +0900

    Fixed bug

--- fastlog.go
+++ fastlog.go
@@ -8,47 +8,45 @@ func Log10(n uint64) int {
 	switch {
 	case n == 0:
 		return 0
-	case n < 10:
+	case n < 1e1:
 		return 1
-	case n < 100:
+	case n < 1e2:
 		return 2
-	case n < 100:
+	case n < 1e3:
 		return 3
-	case n < 1000:
+	case n < 1e4:
 		return 4
-	case n < 10000:
+	case n < 1e5:
 		return 5
-	case n < 100000:
+	case n < 1e6:
 		return 6
-	case n < 1000000:
+	case n < 1e7:
 		return 7
-	case n < 10000000:
+	case n < 1e8:
 		return 8
-	case n < 100000000:
+	case n < 1e9:
 		return 9
-	case n < 1000000000:
+	case n < 1e10:
 		return 10
-	case n < 10000000000:
+	case n < 1e11:
 		return 11
-	case n < 100000000000:
+	case n < 1e12:
 		return 12
-	case n < 1000000000000:
+	case n < 1e13:
 		return 13
-	case n < 10000000000000:
+	case n < 1e14:
 		return 14
-	case n < 100000000000000:
+	case n < 1e15:
 		return 15
-	case n < 1000000000000000:
+	case n < 1e16:
 		return 16
-	case n < 10000000000000000:
+	case n < 1e17:
 		return 17
-	case n < 100000000000000000:
+	case n < 1e18:
 		return 18
-	case n < 1000000000000000000:
+	case n < 1e19:
 		return 19
-	case n < 10000000000000000000:
-		return 20
 	default:
-		return 21
+		return 20
 	}
 }
--- fastlog_test.go
+++ fastlog_test.go
@@ -11,20 +11,48 @@ type testCase struct {
 	expected int
 }
 
+func assertLog10(t *testing.T, n uint64) {
+	actual := Log10(n)
+	expected := len(fmt.Sprint(n))
+	if actual != expected {
+		t.Errorf("expected Log10(%d) = %d, but got %d", n, expected, actual)
+	}
+}
+
 func Test(t *testing.T) {
-	cases := []testCase{
-		{0, 0},
-		{1, 1},
-		{9, 1},
-		{10, 2},
-		{18446744073709551615, 21},
+	cases := []uint64{
+		10,
+		1e3,
+		1e4,
+		1e5,
+		1e6,
+		1e7,
+		1e8,
+		1e9,
+		1e10,
+		1e11,
+		1e12,
+		1e13,
+		1e14,
+		1e15,
+		1e16,
+		1e17,
+		1e18,
+		1e19,
+		18446744073709551615,
 	}
 
-	for _, c := range cases {
-		actual := Log10(c.input)
-		if actual != c.expected {
-			t.Errorf("expected Log10(%d) = %d, but got %d", c.input, c.expected, actual)
+	assertLog10(t, 1)
+
+	for _, x := range cases {
+		for _, n := range []uint64{x, x - 1, x / 2} {
+			assertLog10(t, n)
 		}
+	}
+
+	actual := Log10(0)
+	if actual != 0 {
+		t.Errorf("expected Log10(0) = 0, but got %d", actual)
 	}
 }
 
